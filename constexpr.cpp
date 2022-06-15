fb5d5fdebbb0e0e3ab4e6e8d65fc743fd8b65b4e
rawrex <rawrex@proton.me>
2022-06-15 13:27:21 +0700

    Separate, general, constexpr note initialized.

---
+++ constexpr.cpp
@@ -0,0 +1,4 @@
+// In general, the constexpr specifier declares 
+// that it is possible to evaluate the value of the function or variable at compile time.
+// Such variables and functions can then be used where only compile time constant expressions are allowed
+// (provided that appropriate function arguments are given).
--- constexpr_ctor.cpp
+++ constexpr_ctor.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-// In general, the constexpr specifier declares 
-// that it is possible to evaluate the value of the function or variable at compile time.
-// Such variables and functions can then be used where only compile time constant expressions are allowed
-// (provided that appropriate function arguments are given).
-
 // Ctors can't be const, but ctors in a literal class can be constexpr functions
 // (Recall that a literal class must provide at least one constexpr ctor)
 
