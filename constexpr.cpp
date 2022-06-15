44cdce461a9d3a2617a8a74490424d3e10086365
rawrex <rawrex@proton.me>
2022-06-15 13:53:20 +0700

    Minor addition to the note.

--- constexpr.cpp
+++ constexpr.cpp
@@ -1,4 +1,13 @@
+#include <string>
+#include <iostream>
+
 // In general, the constexpr specifier declares 
 // that it is possible to evaluate the value of the function or variable at compile time.
 // Such variables and functions can then be used where only compile time constant expressions are allowed
 // (provided that appropriate function arguments are given).
+
+// A constexpr in an object declaration or non-static member function (until C++14) implies const.
+// A constexpr in a function or static data member (since C++17) declaration implies inline.
+
+
+int main() {}
