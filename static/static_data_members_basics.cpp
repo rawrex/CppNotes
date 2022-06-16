b2cdf0dc7c7789638ff2e345ba419abe17f2ddea
rawrex <rawrex@proton.me>
2022-06-16 10:46:36 +0700

    Started to intorduce more structure to the repository. Notes are not always come in groups, so there will be a lot of single files.

--- static/static_data_members_basics.cpp
+++ static/static_data_members_basics.cpp
@@ -4,11 +4,14 @@
 class YoungGroup
 {
 	static unsigned group_max_age;
+
+	// Note that init_group_max_age is private
 	static unsigned init_group_max_age()
 	{
 		const unsigned init_age = 25;	
 		return init_age;
 	}
+
 	unsigned population;
 	std::string name;
 
@@ -27,8 +30,12 @@ public:
 	}
 };
 
+// As with any other member definition,
+// a static data member definition may access the private members of its class
+// in this case, the init_group_max_age()
 unsigned YoungGroup::group_max_age = init_group_max_age();
 
 int main() {
+	YoungGroup::max_age(33);
 	std::cout << YoungGroup::max_age() << std::endl;
 }
