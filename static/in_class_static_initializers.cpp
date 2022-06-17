b02d7f01ff22711aa481cbb37fe3b293766548f6
rawrex <rawrex@proton.me>
2022-06-17 11:08:10 +0700

    Added info about outside definition in case of in-class initializer for a static member.

--- static/in_class_static_initializers.cpp
+++ static/in_class_static_initializers.cpp
@@ -14,15 +14,15 @@ struct Foo {
 	// Must be const
 	// static int data_a = 63;
 
-	// OK, we may provide in-class initializer for data members that have const integral type
+	// OK, we may provide in-class initializer for data members that have const integer type
 	const static int data_b = 42;
+
+	// We must provide an in-class initializer for a static constexpr member 
+	constexpr static int data_c = 21;
 
 	// error: ‘constexpr’ needed for in-class initialization of static data member 
 	// Not an integral type and not a constexpr
 	// const static Literal data_d = {1.4141, 1.7320};
-
-	// We must provide an in-class initializer for a static constexpr member 
-	constexpr static int data_c = 21;
 
 	// OK, the member is a static constexpr and the initializer is a constexpr
 	// Such a member is itself a constant expression 
@@ -31,6 +31,23 @@ struct Foo {
 
 };
 
+// If we use a static memeber only in contexts where the complier can substitute the member's value,
+// then we do not need to separetely define the member.
+// However, if we use the member in other context in which the value cannot be substituted,
+// then we must provide a definition for that member:
+//
+// E.g. If we pass the Foo::data_b to a function that takes a const int&:
+void print(const int& i) { std::cout << i << std::endl; }
+
+// If the initializer if provided in the class,
+// the member's definition outside must not specify an initial value:
+const int Foo::data_b;
+
+
 int main() {
-	std::cout << Foo::data_d.data_1 << std::endl;
+
+	// If we do not define the Foo::data outside of the class' scope, we'll get:
+	// undefined reference to `Foo::data_b'
+	print(Foo::data_b);
+	
 }
