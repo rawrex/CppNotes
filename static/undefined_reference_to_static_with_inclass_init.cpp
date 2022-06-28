e5cd6aadd5b253595eabcc18a4a174987a7480fc
rawrex <rawrex@proton.me>
2022-06-28 11:43:44 +0700

    Added an example of a tricky static data member use case.

--- static/undefined_reference_to_static_with_inclass_init.cpp
+++ static/undefined_reference_to_static_with_inclass_init.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 struct Foo {
-
 	// All good
 	const static bool b = true;
 	const static size_t size = 42;
@@ -10,30 +9,16 @@ struct Foo {
 	const static long ln = 105;
 	const static long long lnln = 126;
 	const static wchar_t wch = 147;
-
 	// Failing member
 	const static int integer = 21;
-
 };
-
-// If we do supply the outside definition, looking in the object file will show us
-// That Foo::integer is in a read only data seciton: "R Foo::integer"
-// const int Foo::integer; 
-
-// Same for any other data member of the Foo class
-// const unsigned Foo::uint; 
-
-// But if we do not supply outside definition for the Foo::integer
-// We'll get: "U Foo::integer". Which means that the member is not defined.
-// While we do not get the same status for any other data member in Foo
-
 
 // The test context for a member to be used where it won't be a simple substitution of the value
 void print(const int& i) { std::cout << "print(" << i << ')' << std::endl; }
 
 int main() {
-	
-	// These are OK:
+	// These are OK due to the implicit conversions and introduced temporaries as a result.
+	// So, none of these calls tries to fetch the address of an undefined statis data member
 	print(Foo::b);
 	print(Foo::size);
 	print(Foo::uint);
@@ -43,11 +28,6 @@ int main() {
 	print(Foo::wch);
 
 	// undefined reference to `Foo::integer'
+	// Since we pass the exact type, so no conversions, no temporaries{
 	print(Foo::integer);
 }
-
-
-// Related threads:
-// https://stackoverflow.com/questions/5391973/undefined-reference-to-static-const-int
-// https://stackoverflow.com/questions/272900/undefined-reference-to-static-class-member
-// https://stackoverflow.com/questions/5508182/static-const-int-causes-linking-error-undefined-reference
