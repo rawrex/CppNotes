484c9e0cf0f17a3817030de455037e13e7a3b94b
rawrex <rawrex@proton.me>
2022-07-04 10:38:22 +0700

    Stated the cases in which an output buffer may be flushed.

--- io_library/output_buffer.cpp
+++ io_library/output_buffer.cpp
@@ -3,6 +3,19 @@
 #include <chrono>
 #include <thread>
 
+// The OS might store the data sent to the output stream in a buffer
+// and then combine several output operations in one system level wirte as a preformance optimizaiton.
+// 
+// There are several conditions that cause the buffer to be flushed:
+//	1. The program completes (normally). All output buffers are flushed on the return from main()
+//	2. Buffer may become full, in which case it must be flushed before the next write to it
+//	3. We can explicitly flush the buffer, using the stream manipulators
+//	4. We can use the unitbuf manipulator to set the stream's internal state
+//	   to empty the bufffer after each operation.
+//	   The unitbuf is set for std::cerr, so writes to it are written immediately.
+//	5. An output stream may be tied to another stream.
+//	   If so, the output stream is flushed every time the stream it tied to is read or written.
+
 unsigned int microsecond = 1000000;
 unsigned sleep_duration = 3;
 
