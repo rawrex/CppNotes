f3d2e2be6df04ee5425509557effa73828eeee03
rawrex <rawrex@proton.me>
2022-07-04 11:39:04 +0700

    Notes on the output buffer manipulators were added.

--- io_library/manipulators.cpp
+++ io_library/manipulators.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-
+void sleep(unsigned duration = 3)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(duration));
+}
 
 int main() {
 
 	// Ends the current line and flushes the buffer
-	std::cout << "endl" << std::endl;
+	std::cout << "endl" << std::endl;	// No need for \n at the end of the message
+	std::cout << "...had no manipulators, printed with the next flush";
+	sleep();
 
 	// Flushes the stream but does not adds any characters to the output
-	std::cout << "flush" << std::endl;
+	std::cout << "\nflush\n" << std::flush;
+	std::cout << "...had no manipulators, printed with the next flush";
+	sleep();
 
 	// Inserts a null character into the buffer and then flushes it
-	std::cout << "ends" << std::endl;
+	std::cout << "\nends\n" << std::ends;
+	std::cout << "...had no manipulators, printed with the next flush";
+	sleep();
+
+	// Modify the internal state of the stream. Flush the stream on every output operation
+	std::cout << std::unitbuf;
+	std::cout << "\nunitbuf\n";
+	std::cout << "...had no manipulators, but printed immediately";
+	sleep();
+
+	// Restore the state of the stream to the system-managed buffer flushing:
+	std::cout << std::nounitbuf;
+	std::cout << "\nnounitbuf\n";
+	std::cout << "...had no manipulators, printed on the program termination";
+	std::cout << '\n';
+	sleep();
 
 }
--- io_library/output_buffer.cpp
+++ io_library/output_buffer.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unistd.h>
+//#include <unistd.h>
 #include <chrono>
 #include <thread>
 
