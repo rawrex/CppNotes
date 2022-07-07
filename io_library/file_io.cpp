f36d2e7bfb4165585da597e9f15c2c2292fad0f6
rawrex <rawrex@proton.me>
2022-07-07 12:28:38 +0700

    Renamed the file for it to be more explanatory.

--- io_library/file_io.cpp
+++ io_library/file_io.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 
 void print(const std::string& msg) { std::cout << msg << std::endl; }
-const std::string default_filename("test.file");
+const std::string test_filename("test.file");
+const std::string another_filename("another_test.file");
 
-void readFile(const std::string & filename = default_filename)
+void readFile(const std::string & filename = test_filename)
 {
 	// Create a file stream and open the file.
 	// Default file mode depends on the type of the stream, thus read-only.
@@ -39,6 +40,7 @@ void openAnotherFileSameStream(std::fstream& fstream, const std::string& new_fil
 	// We can associate another file with the same file stream
 	// In order to do so we first need to close the existing file,
 	// Once it is closed, we can open a new file.
+	std::fstream fstream(test_filename);
 	fstream.close();
 	fstream.open(new_filename);
 	if(fstream)
@@ -58,8 +60,8 @@ void alreadyOpenedFile()
 	// Thus, in such case, we will need to handle such situation before further processing.
 
 	std::fstream fstream;				// Create a file stream, no file is associated.
-	fstream.open(default_filename);		// Explicitly associate a file with the file stream
-	fstream.open(default_filename); 	// Second call to open will set the failbit,
+	fstream.open(test_filename);		// Explicitly associate a file with the file stream
+	fstream.open(test_filename); 	// Second call to open will set the failbit,
 										// Rendering the stream unusable for now
 	if(fstream.fail())
 	{
@@ -72,24 +74,24 @@ void alreadyOpenedFile()
 void closeClosedFile()
 {
 	// We can close a file stream more than once. No error.
-	std::fstream file(default_filename);
+	std::fstream file(test_filename);
 	file.close();
 	file.close();
 }
 		
 int main() {
 	// readFile("test.file");
-	openClose(default_filename);
+	openClose(test_filename);
 
 	// Open a file for this stream to open another file for it later
 	std::fstream ifile("test_2.file");	
-	openAnotherFileSameStream(ifile, default_filename);
+	openAnotherFileSameStream(ifile, test_filename);
 
 	// Try to call open on an "opened" file stream, fail, clear the state
 	alreadyOpenedFile();
 
 	// Try to use the stream again
-	openAnotherFileSameStream(ifile, default_filename);
+	openAnotherFileSameStream(ifile, test_filename);
 
 	closeClosedFile();
 }
