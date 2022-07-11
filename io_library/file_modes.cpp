848ee0045859df27cd0c2adbb6894eeb5234d87d
rawrex <rawrex@proton.me>
2022-07-11 12:08:43 +0700

    Basic info was added on file modes.

--- io_library/file_modes.cpp
+++ io_library/file_modes.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <fstream>
 
-// Each file stream has an associated file mode
+// Each file stream has an associated file mode that represents how the file may be used
+//
+//	in			- open for input
+//	out			- open for output
+//	app			- append, seek to the end of the file before every write
+//	ate			- seek to the end immediately after the open
+//	binary		- do IO operations in binary mode
+//	trunc		- truncate the file
 
 int main() {
 
