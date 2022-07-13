9a9e56b06666e3a0d479797bb9098f997c883dce
rawrex <rawrex@proton.me>
2022-07-13 12:33:52 +0700

    Some basic info on the sstream types.

--- io_library/string_basics.cpp
+++ io_library/string_basics.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <sstream>
 
+// The sstream header defined types which we can use to do in-memory IO
+// These types read and write to an underlying std::string as if it was an IO stream.
+
+// We have the std::istringstream which reads a string;
+// The std::ostringstream which writes to a string;
+// And the std::stringstream which does both.
+
+
+
 int main() {
 
 }
