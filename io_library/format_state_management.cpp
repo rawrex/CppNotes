12112dca7da59322b2e94c0fdae395dd66d2b104
rawrex <rawrex@proton.me>
2022-07-14 13:02:30 +0700

    Basic info added.

--- io_library/format_state_management.cpp
+++ io_library/format_state_management.cpp
@@ -2,7 +2,13 @@
 
 // In addition to condition state, each iostream object maintains a formatting state.
 // Which contorls how IO is formatted.
+// The library defines a set of manipulators (an object or a function)
+// That we can use to modify the state, as an operand to an IO operator.
+
 
 int main() {
+
+	// A manipulator returns the stream to which it is applied, so we can chain operations:
+	std::cout << std::boolalpha << true << std::noboolalpha << ' ' << false << std::endl;
 
 }
