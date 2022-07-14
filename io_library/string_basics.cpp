6a0e60fca0dbfd20eea7773a7446cd4a1c2940e7
rawrex <rawrex@proton.me>
2022-07-14 11:45:42 +0700

    A basic usage example was added.

--- io_library/string_basics.cpp
+++ io_library/string_basics.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 // The sstream header defined types which we can use to do in-memory IO
 // These types read and write to an underlying std::string as if it was an IO stream.
@@ -19,14 +20,53 @@
 // These types inherit from the iostream header types (just like fstream).
 // These types also introduce some specific operations as well (just like fstream).
 
-std::string string_a("Foo bar");
-std::stringstream string_stream(string_a);		// Holds a copy of the str, this ctor is explicit
 
-std::string string_b = string_stream.str();		// Returns a copy of the internal string
 
-std::string string_c("Baz qux");
-string_stream.str(string_c);					// Copies string into the stream, returns void
+void example()
+{
+	// Our custom data type
+	struct Widget
+	{
+		std::string id;
+		std::vector<std::string> aliases;
+	};
+	std::vector<Widget> data;
+	std::string line;
+	std::string word;
+
+	// Get input from a user and fill the data container accordingly
+	while(getline(std::cin, line))
+	{
+		Widget widget;
+		std::istringstream iss(line);			// Prepare the sstream, copy the line into it
+		iss >> widget.id;						// Get the id from the stream first
+		while(iss >> word)						// Then get all the aliases
+			widget.aliases.push_back(word);
+
+		data.push_back(widget);
+	}
+
+	// Print the contents of the now populated data
+	for (const auto& wid : data)
+	{
+		std::cout << wid.id << std::endl;
+		for (const auto& alias : wid.aliases)
+			std::cout << alias << ' ';
+		std::cout << std::endl;
+	}
+}
+				
 
 int main() {
+
+	std::string string_a("Foo bar");
+	std::stringstream string_stream(string_a);		// Holds a copy of the str, this ctor is explicit
+
+	std::string string_b = string_stream.str();		// Returns a copy of the internal string
+
+	std::string string_c("Baz qux");
+	string_stream.str(string_c);					// Copies string into the stream, returns void
+
+	example();
 
 }
