d76e3557255fefaddbf96453e60be3408c0838e8
rawrex <rawrex@proton.me>
2022-07-14 11:05:54 +0700

    A basic info is layed out.

--- io_library/string_basics.cpp
+++ io_library/string_basics.cpp
@@ -4,6 +4,14 @@
 // The sstream header defined types which we can use to do in-memory IO
 // These types read and write to an underlying std::string as if it was an IO stream.
 
+// At the low level,
+// the class essentially wraps a raw string device implementation of std::basic_stringbuf
+// into a higher-level interface of std::basic_iostream.
+
+// There are two specializations for common character types are also defined:
+//	- stringstream 		basic_stringstream< char >
+//	- wstringstream 	basic_stringstream< wchar_t >
+
 // We have the std::istringstream which reads a string;
 // The std::ostringstream which writes to a string;
 // And the std::stringstream which does both.
