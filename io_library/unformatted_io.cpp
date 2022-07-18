475ac9ecec8a1c3dfb43933eb8ff1e8112f94468
rawrex <rawrex@proton.me>
2022-07-18 10:41:35 +0700

    Initial commit on unformatted IO.

---
+++ io_library/unformatted_io.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Several unformatted IO operations deal with a stream one byte at a time.
+// The unformatted IO operations "get" and "put" read and write characters one at a time.
+// These read (not ignore) whitespace.
+
+int main() {
+
+	// 
+
+}
