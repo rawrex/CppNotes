225d7a54fb8442e8dc24e9717d7a52de14a9f861
rawrex <rawrex@proton.me>
2022-07-18 11:22:01 +0700

    Info on putting back onto an input stream added.

--- io_library/unformatted_io.cpp
+++ io_library/unformatted_io.cpp
@@ -18,6 +18,16 @@ void example_get_put()
 		std::cout.put(ch);
 }
 
+
+// Sometimes we need to read a character from the stream just to put it back onto the stream:
+// peek(), 		returns the copy of the next character on the stream,
+//				does not change the stream.
+// unget(),		back up, so that the value that was last returned is still on the stream,
+//				we can use unget() even if we do not know what value was last taken from the stream.
+// putback(),	returns the last value read from the stream,
+// 				but takes the arg that must be the same as the one that was last read,
+//				in other words, put back what you just took, knowing what you just have taken.
+
 int main() {
 
 	// 
