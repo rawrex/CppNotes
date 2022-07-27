974ed5f69e8db88f54b44222ea8d003dda31a097
rawrex <rawrex@proton.me>
2022-07-27 12:20:59 +0700

    Minore reformatting.

--- io_library/multibyte_io.cpp
+++ io_library/multibyte_io.cpp
@@ -9,18 +9,18 @@ void print(const std::string& msg)
 
 // We can use these for speed, but they are error-prone.
 // They require us to allocate and manage the character arrays as buffers.
-//
+
 // input_stream.get(sink, size, delimiter);		reads characters up to the "size" from "input_stream"
 //												stores them in the character array beginning at the address
 //												pointed to by the "sink";
 //												reads until "size", "delimiter", or EOF is met;
 //												if "delimiter" is present, it is left on the stream 
-//
+
 // input_stream.getline(sink, size, delim);		same as above, but reads and discards the "delim"
-//
+
 // input_stream.read(sink, size);				reads up to "size" bytes into the character array "sink";
 //												returns "input_stream"
-//
+
 // input_stream.gcount();						returns the number of bytes read from the "input_stream"
 //												by the last call to an unformatted read operation,
 //												or the maximum representable value of std::streamsize 
@@ -42,15 +42,17 @@ void print(const std::string& msg)
 //		- unget()
 //		- peek()
 //
+// So, note, it is essential to call gcount() before any of these operations for it to return the intended value.
+
 // input_stream.ignore(size, delimiter);		reads and ignores at most "size" characters up to,
 //												up to (not including) "delimiter";
 //												unline other operations, has default arguments:
 //												"size" = 1, "delimiter" = EOF
-//
+
 // output_stream.write(source, size);			writes "size" bytes from the character array "source"
 //												into "output_stream"; 
 //												returns "output_stream"
-//
+
 // Note, it is not uncommon to forget to remove the delimitre from the stream
 
 
