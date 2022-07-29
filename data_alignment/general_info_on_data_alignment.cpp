adc69ff086efa50587affaad3e5ea0618f58d545
rawrex <rawrex@proton.me>
2022-07-29 17:07:52 +0700

    Initial commit on the general info note.

---
+++ data_alignment/general_info_on_data_alignment.cpp
@@ -0,0 +1,5 @@
+// Data structure alignment is the way data is arranged and accessed in computer memory.
+// It consists of three separate but related issues:
+//	- data alignment,
+//	- data structure padding,
+//	- packing.
