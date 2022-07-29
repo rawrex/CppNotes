8a6509114fb942066aeb8be158a743b58e00e068
rawrex <rawrex@proton.me>
2022-07-29 17:14:54 +0700

    Info on alignment and padding added.

--- data_alignment/general_info_on_data_alignment.cpp
+++ data_alignment/general_info_on_data_alignment.cpp
@@ -1,5 +1,17 @@
 // Data structure alignment is the way data is arranged and accessed in computer memory.
 // It consists of three separate but related issues:
-//	- data alignment,
-//	- data structure padding,
-//	- packing.
+//	- Data alignment,
+//	- Data structure padding,
+//	- Packing
+
+// The CPU in modern computer hardware performs reads and writes 
+// Most efficiently when the data is naturally aligned.
+//
+// "Natural" alignment means aligned to its own type width.
+// So, the first one, Data Aliginment is the aligning of elements according to their natural alignment.
+//
+// To ensure natural alignment, it may be sometimes necessary to insert some padding 
+// between structure elements or after the last element of a structure.
+// E.g. on a 32-bit machine,
+// A data structure containing a 16-bit value followed by a 32-bit value could have 16 bits of padding 
+// between the 16-bit value and the 32-bit value to align the 32-bit part on a 32-bit boundary.
