fe369c8c742ccbae3da1755bd4e173d799f64686
rawrex <rawrex@proton.me>
2022-08-01 10:53:20 +0700

    A general info added.

--- io_library/random_access_to_stream.cpp
+++ io_library/random_access_to_stream.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+// In general, a system will support a random access to the data on streams
+// The library provides a set of funcitons to seek to a given location 
+// and to tell the current location in the associated stream.
+//
+// So, note the IO is system dependent.
+
+
 int main() {
 
 }
