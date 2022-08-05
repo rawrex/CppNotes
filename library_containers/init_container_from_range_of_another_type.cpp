bfa6ebdc27ae2195c67cf975956f096128acd6fc
rawrex <rawrex@proton.me>
2022-08-05 10:41:08 +0700

    Basic info on the ctor from a range of convertable type added.

--- library_containers/init_container_from_range_of_another_type.cpp
+++ library_containers/init_container_from_range_of_another_type.cpp
@@ -13,6 +13,10 @@ void print(const T& container)
 int main() {
 
 	std::vector<const char*> vec = {"foo", "bar", "baz"};
+	
+	// The ctor takes two iterators denoting a range of elements
+	// In such construction, there's no requirements for the types of containers
+	// Or types of their elements to match (they just have to be convertable).
 	std::list<std::string> lt(vec.cbegin(), vec.cend());
 	print(lt);
 	
