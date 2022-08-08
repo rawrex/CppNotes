3cfa9e07205d7b0fcc05a9ae4a7801d4908e44c0
rawrex <rawrex@proton.me>
2022-08-08 11:34:49 +0700

    More examples of the assign() use.

--- library_containers/assign_mem_funciton.cpp
+++ library_containers/assign_mem_funciton.cpp
@@ -30,9 +30,30 @@ void assign_diff_size_diff_types_example()
 	vec.assign(lt.cbegin(), lt.cend());
 	print(vec);
 }
+void assign_refers_to_itself()
+{
+	std::vector<int> vec{1,2,3};
+
+	// Note, since the existing elements are replaced
+	// The arguments to assign must not refer to the cont. on which assign is called
+	auto beg = vec.cbegin(), end = vec.cend();
+	vec.assign(beg, end);
+
+	print(vec);
+}
+void assign_takes_integral_value_and_elem_value_example()
+{
+	std::vector<int> vec{1,2,3};
+
+	vec.assign(5, -1);
+	print(vec);
+}
+
 
 int main() {
 	assign_same_size_diff_types_example();
 	assign_diff_size_diff_types_example();
+	assign_refers_to_itself();
+	assign_takes_integral_value_and_elem_value_example();
 }
 
