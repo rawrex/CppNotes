f77d7c82fe7f8a3306fe9ebd2aa05d1892343d06
rawrex <rawrex@proton.me>
2022-08-15 12:49:40 +0700

    Started to lay out basics.

--- library_containers/string.cpp
+++ library_containers/string.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <string>
+
+void additional_ways_to_construct_string()
+{
+	// In addition to standard container ctor, string has few special forms
+	
+}
 
 int main() {
 
