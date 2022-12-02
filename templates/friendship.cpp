96ec2762dd6f701e7b3086fb078cbd76fa70b365
rawrex <rawrex@proton.me>
2022-12-02 12:13:58 +0700

    Added an example of non-templated friendship and more comments.

--- templates/friendship.cpp
+++ templates/friendship.cpp
@@ -1,19 +1,30 @@
+// A non-template class, if declared as a friend,
+// will be a friend to all instantiations of the template class
+class FriendToAllInstances;
+
+// A non-template function, if declared as a friend,
+// will be a friend to all instantiations of the template class
+void function();
+
 // Forward declare a template class that we're going to befriend with the Bar
 template <typename Type>
-class Friend;
+class FriendToOneInstance;
 
 // Needed to declare the equality operator
 template <typename Type>
 class MyClass;
 
 template <typename Type>
-bool operator==(const MyClass<T>&, const MyClass<T>&);
+bool operator==(const MyClass<Type>&, const MyClass<Type>&);
 
 template <typename Type>
 class MyClass
 {
-	// One-to-one friendship example (both the operator and the Foo)
-	friend class Friend<Type>;
+	// All instances friendsip
+	friend void function();
+
+	// One-to-one friendship (both the operator and the Foo)
+	friend class FriendToOneInstance<Type>;
 	friend bool operator==<Type>(const MyClass<Type>&, const MyClass<Type>&);
 };
 
