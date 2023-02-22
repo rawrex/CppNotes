241cef9cc992a152a1f5577e84fb8f672593f0cb
rawrex <rawrex@proton.me>
2023-02-22 22:57:50 +0700

    Added few examples of the tuple auxilary functions.

--- project_files/tuple_basics/tuple_basics.cpp
+++ project_files/tuple_basics/tuple_basics.cpp
@@ -1,7 +1,29 @@
 #include <tuple>
 #include <iostream>
+#include <typeinfo>
 
 int main()
 {
+	std::tuple<const char*, std::string, std::wstring> strings_a { "foo", "bar", L"baz" };
+	const auto strings_b = std::make_tuple("foo", "bar", L"baz");
+
+	std::cout << "strings_b size: " << std::tuple_size<decltype(strings_b)>::value << std::endl;
+
+	std::cout << std::get<0>(strings_b) << std::endl;
+	std::cout << std::get<1>(strings_b) << std::endl;
+	std::wcout << std::get<2>(strings_b) << std::endl;
+
+	std::cout << typeid(std::tuple_element<0, decltype(strings_a)>::type).name() << std::endl;
+	std::cout << typeid(std::tuple_element<1, decltype(strings_a)>::type).name() << std::endl;
+	std::cout << typeid(std::tuple_element<2, decltype(strings_a)>::type).name() << std::endl;
+
+
+	std::tuple_element<0, decltype(strings_b)>::type first_element {};
+	std::tuple_element<1, decltype(strings_b)>::type second_element {};
+	std::tuple_element<2, decltype(strings_b)>::type third_element {};
+
+	std::cout << typeid(std::tuple_element<0, decltype(strings_b)>::type).name() << std::endl;
+	std::cout << typeid(std::tuple_element<1, decltype(strings_b)>::type).name() << std::endl;
+	std::cout << typeid(std::tuple_element<2, decltype(strings_b)>::type).name() << std::endl;
 
 }
