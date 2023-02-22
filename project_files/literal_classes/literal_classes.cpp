809310d3fd147af5de77464ca79d33cda45679d4
rawrex <rawrex@proton.me>
2023-02-22 14:30:37 +0700

    Introduced the solution structured as a collection of projects. With a project for each topic demo.

--- project_files/literal_classes/literal_classes.cpp
+++ project_files/literal_classes/literal_classes.cpp
@@ -1,4 +1,4 @@
-// CppNotes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// literal_classes.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
