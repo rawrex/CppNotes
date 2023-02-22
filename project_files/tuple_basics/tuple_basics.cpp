e8260133cc308c6b9e8b7d61fa3ba029d10da9f1
rawrex <rawrex@proton.me>
2023-02-22 23:36:36 +0700

    Added few exapmles with std::tie and std::tuple_cat.

--- project_files/tuple_basics/tuple_basics.cpp
+++ project_files/tuple_basics/tuple_basics.cpp
@@ -1,8 +1,9 @@
 #include <tuple>
+#include <utility>
 #include <iostream>
 #include <typeinfo>
 
-int main()
+void basicTupleUses()
 {
 	std::tuple<const char*, std::string, std::wstring> strings_a { "foo", "bar", L"baz" };
 	const auto strings_b = std::make_tuple("foo", "bar", L"baz");
@@ -26,4 +27,23 @@ int main()
 	std::cout << typeid(std::tuple_element<0, decltype(strings_b)>::type).name() << std::endl;
 	std::cout << typeid(std::tuple_element<1, decltype(strings_b)>::type).name() << std::endl;
 	std::cout << typeid(std::tuple_element<2, decltype(strings_b)>::type).name() << std::endl;
+}
+
+void tupleCat()
+{
+	char ch = 'x';
+	double pi = 3.1415;
+
+	auto t1 = std::make_tuple<std::string, int>("foo", 1);
+	auto t2 = std::make_tuple<size_t, double, std::pair<int, int>>(0, 3.14, { 11, 22 });
+
+	// t3 will be a tuple with elements as lvalue references to the two local variables ch and pi
+	auto t3 = std::tie(ch, pi);
+
+	auto t = std::tuple_cat(t1, t2, t3);
+}
+
+int main()
+{
+	basicTupleUses();
 }
