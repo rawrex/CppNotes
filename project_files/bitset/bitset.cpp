9f46b787ebb77beb7de025812b421aceb71182e3
rawrex <rawrex@proton.me>
2023-02-23 14:12:20 +0700

    Added an example of defaulted values for the construction from a std::string.

--- project_files/bitset/bitset.cpp
+++ project_files/bitset/bitset.cpp
@@ -9,9 +9,13 @@ void basicExample()
 	std::bitset<sizeof(unsigned) * 8> data(std::numeric_limits<unsigned>::max());
 	std::cout << data << std::endl;
 
-	std::string data_string("xxooxoxo");
-	std::bitset<8> data_from_string(data_string, 0, std::string::npos, 'o', 'x');
-	std::cout << data_from_string << std::endl;
+	std::string data_string_A("11001010");
+	std::bitset<8> data_from_string_A(data_string_A);
+	std::cout << data_from_string_A << std::endl;
+
+	std::string data_string_B("xxooxoxo");
+	std::bitset<8> data_from_string_B(data_string_B, 0, std::string::npos, 'o', 'x');
+	std::cout << data_from_string_B << std::endl;
 }
 
 int main()
