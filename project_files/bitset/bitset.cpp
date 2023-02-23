041ff56b59527340cb349aed53dab8c80f84faba
rawrex <rawrex@proton.me>
2023-02-23 14:25:54 +0700

    Added clarification comments.

--- project_files/bitset/bitset.cpp
+++ project_files/bitset/bitset.cpp
@@ -5,11 +5,13 @@
 
 void basicExample()
 {
+	// The bitset size specification must be a constant expression
 	// All ones, "1", with as many bits as the unsigned int type has
 	std::bitset<sizeof(unsigned) * 8> data(std::numeric_limits<unsigned>::max());
 	std::cout << data << std::endl;
 
 	std::string data_string_A("11001010");
+	// The ctors that take a string or char pointer are explicit
 	std::bitset<8> data_from_string_A(data_string_A);
 	std::cout << data_from_string_A << std::endl;
 
