6d4e5e7adc64b6d6e7959015c5481d10f4993800
rawrex <rawrex@proton.me>
2023-02-23 13:54:49 +0700

    Added a basic usage example of the bitset type.

--- project_files/bitset/bitset.cpp
+++ project_files/bitset/bitset.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <bitset>
+#include <string>
+#include <limits>
+
+void basicExample()
+{
+	// All ones, "1", with as many bits as the unsigned int type has
+	std::bitset<sizeof(unsigned) * 8> data(std::numeric_limits<unsigned>::max());
+	std::cout << data << std::endl;
+
+	std::string data_string("xxooxoxo");
+	std::bitset<8> data_from_string(data_string, 0, std::string::npos, 'o', 'x');
+	std::cout << data_from_string << std::endl;
+}
 
 int main()
 {
-
+	basicExample();
 }
