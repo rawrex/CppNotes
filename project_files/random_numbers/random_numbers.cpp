33b051c955cbc26edfe777e85ddb52909dfd0472
rawrex <rawrex@proton.me>
2023-02-26 13:45:42 +0700

    Added a basic example of the legacy usage.

--- project_files/random_numbers/random_numbers.cpp
+++ project_files/random_numbers/random_numbers.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 #include <random>
+
+
+void legacyExample()
+{
+	// Seeds the pseudo-random number generator used by rand()
+	// Here, we use the current system time
+	srand(time(nullptr));
+
+	// Returns a pseudo-random integer value between ​0​ and RAND_MAX (inclusively)
+	auto random_number = rand();
+	std::cout << random_number << std::endl;
+}
+
+
 
 int main()
 {
-
+	legacyExample();
 }
