045fbc50377842b785d3cdc8065685fd82f1bf7e
rawrex <rawrex@proton.me>
2023-02-28 12:57:39 +0700

    Few minor text changes and activated the call to the unformatted_io_main().

--- src/io_library/main.cpp
+++ src/io_library/main.cpp
@@ -1,9 +1,9 @@
+#include "format_state_management.h"
 #include "buffer_management.h"
 #include "condition_states.h"
 #include "condition_states_examples.h"
 #include "file_basics.h"
 #include "file_modes.h"
-#include "format_state_management.h"
 #include "multibyte_io.h"
 #include "output_buffer.h"
 #include "random_access_to_stream.h"
@@ -13,12 +13,14 @@
 
 int main()
 {
+	// format_state_management_main();
+	unformatted_io_main();
+
 	// buffer_management_main();
 	// condition_states_main();
 	// condition_states_examples_main();
 	// file_basics_main();
 	// file_modes_main();
-	// format_state_management_main();
 	// multibyte_io_main();
 	// output_buffer_main();
 	// random_access_to_stream_main();
