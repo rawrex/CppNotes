4fae98943739aa430da52519c19d463c58f3a8e3
rawrex <rawrex@proton.me>
2023-02-28 12:58:19 +0700

    Few minor text corrections.

--- src/io_library/unformatted_io.cpp
+++ src/io_library/unformatted_io.cpp
@@ -20,16 +20,17 @@ void example_get_put()
 }
 
 // Sometimes we need to read a character from the stream just to put it back onto the stream:
-// peek(), 		returns the copy of the next character on the stream,
-//				does not change the stream.
-// unget(),		back up, so that the value that was last returned is still on the stream,
-//				we can use unget() even if we do not know what value was last taken from the stream.
-// putback(),	returns the last value read from the stream,
-// 				but takes the arg that must be the same as the one that was last read,
-//				in other words, put back what you just took, knowing what you just have taken.
+// peek(),			returns the copy of the next character on the stream,
+//					does not change the stream.
+// unget(),			back up, so that the value that was last returned is still on the stream,
+//					we can use unget() even if we do not know what value was last taken from the stream.
+// putback(arg),	returns the last value read from the stream,
+//					but takes the arg that must be the same as the one that was last read,
+//					in other words, put back what you just took, knowing what you just have taken.
 //
-// We ate guaranteed to be able to put back at worst one value before the next read;
+// We are guaranteed to be able to put back at worst one value before the next read;
 // Note, we are not guaranteed to be able to call putback() ot unget() successively without a read.
+
 void example_peek()
 {
 	char ch_peek = '\0';
@@ -123,5 +124,5 @@ void unformatted_io_main()
 	// example_peek();
 	// example_unget();
 	// example_putback();
-	example_int_returns();
+	// example_int_returns();
 }
